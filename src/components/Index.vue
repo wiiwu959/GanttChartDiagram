a2b28d3099d8f06530890253be816808aa846511
子祐 曾 <pompelmous429@gmail.com>
2019-12-09 15:40:52 +0800

    index toolbar

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -3,6 +3,8 @@
     <!-- 這裡是首頁 -->
     <header class="header">
       <!-- 這裡是最上面放logo的地方 -->
+
+      
       <img src="../assets/illustration/header.png" alt />
     </header>
     <!-- router-view 會依據網址改變內容 -->
