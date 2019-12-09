45adc0408a76fd674c0606fa9df1e2031aceae93
子祐 曾 <pompelmous429@gmail.com>
2019-12-09 18:42:43 +0800

    fix index

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,7 +2,9 @@
   <div id="index">
     <!-- 這裡是首頁 -->
     <header class="header">
-      <img class="gantt-logo" src="../assets/img/logo.png">
+      <router-link to="/">
+        <img class="gantt-logo" src="../assets/img/logo.png">
+      </router-link>
     </header>
     <!-- router-view 會依據網址改變內容 -->
     <!-- 可以看 router/index.js -->
@@ -19,13 +21,15 @@ export default {
 <style scoped>
 
 .header {
-  height: 50px;
+  height: 80px;
   display: flex;
   align-items: center;
 }
 
 .gantt-logo{
-  margin: left;
+  margin-left: 20px;
+  width: 56px;
+  height: 56px;
 }
 
 </style>
