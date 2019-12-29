fabfce7e48fa464b6e9db2290c526d14475e89cb
clhuang224 <clhuang224@gmail.com>
2019-12-29 09:59:29 +0800

    Finish sign up

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { firestorePlugin } from 'vuefire'
 
+Vue.use(firestorePlugin);
 Vue.config.productionTip = false;
+
+router.beforeEach((to, from, next) => {
+  const isLogin = localStorage.getItem('token') == 'isLogin';
+  if (isLogin) {
+      next();
+  } else {
+      if (to.path !== '/signin' && to.path !== '/signup')
+          next('/signin');
+      else
+          next();
+  }
+});
 
 new Vue({
   el: '#app',
